0521294cbf6a4bc87e3cdd254d11fd82711c443f
ai-fast-track <farid@itecpedia.com>
2022-10-24 12:29:32 -0400

    add color argument to run_on_web_image method

--- maskrcnn_benchmark/engine/predictor_glip.py
+++ maskrcnn_benchmark/engine/predictor_glip.py
@@ -136,7 +136,11 @@ class GLIPDemo(object):
             original_caption, 
             thresh=0.5,
             custom_entity = None,
-            alpha = 0.0):
+            alpha = 0.0,
+            color = 255):
+        
+        self.color = color
+
         predictions = self.compute_prediction(original_image, original_caption, custom_entity)
         top_predictions = self._post_process(predictions, thresh)
 
